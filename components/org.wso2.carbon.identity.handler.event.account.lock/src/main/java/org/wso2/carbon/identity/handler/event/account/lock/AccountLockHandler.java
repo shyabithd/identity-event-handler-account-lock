c53286b8bcb7d1002c2edf6a5b6d315aca3e7e0d
Shyabith <shyabith@WSO2s-MacBook-Pro-5.local>
2018-06-21 10:38:29 +0530

    privilege user list added to disable account locking functionality

--- components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountLockHandler.java
+++ components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountLockHandler.java
@@ -41,6 +41,11 @@ import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.UserStoreManager;
 import org.wso2.carbon.user.core.util.UserCoreUtil;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -54,6 +59,8 @@ public class AccountLockHandler extends AbstractEventHandler implements Identity
     private static ThreadLocal<String> lockedState = new ThreadLocal<>();
 
     private enum lockedStates {LOCKED_MODIFIED, UNLOCKED_MODIFIED, LOCKED_UNMODIFIED, UNLOCKED_UNMODIFIED}
+
+    private List<String> privilegeUserList;
 
     public String getName() {
         return "account.lock.handler";
@@ -83,6 +90,21 @@ public class AccountLockHandler extends AbstractEventHandler implements Identity
         super.init(initConfig);
         AccountServiceDataHolder.getInstance().getBundleContext().registerService
                 (IdentityConnectorConfig.class.getName(), this, null);
+
+        privilegeUserList = new ArrayList<>();
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream in = classLoader.getResourceAsStream("PrivilegeUsers");
+        if (in != null) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+            String line;
+            try {
+                while ((line = rd.readLine()) != null) {
+                    privilegeUserList.add(line);
+                }
+            } catch (IOException e) {
+                log.error("Privilege user file does not exist");
+            }
+        }
     }
 
     public Map<String, String> getPropertyNameMapping() {
@@ -294,7 +316,7 @@ public class AccountLockHandler extends AbstractEventHandler implements Identity
             Map<String, String> newClaims = new HashMap<>();
             newClaims.put(AccountConstants.FAILED_LOGIN_ATTEMPTS_CLAIM, currentFailedAttempts + "");
 
-            if (currentFailedAttempts >= maximumFailedAttempts) {
+            if (!privilegeUserList.contains(userName) && currentFailedAttempts >= maximumFailedAttempts) {
                 //Current falied attempts exceeded maximum allowed attempts. So ther user should be locked.
 
                 newClaims.put(AccountConstants.ACCOUNT_LOCKED_CLAIM, "true");
